1853f8e0f71cca02f73b2efcd1ac3cb4f1cdd979
codingbyasish <161559490+codingbyasish@users.noreply.github.com>
2024-03-14 18:37:01 +0530

    Update Finding_elements.py

--- Finding_elements.py
+++ Finding_elements.py
@@ -15,36 +15,17 @@ def test_open_login():
 
     assert driver.title == "CURA Healthcare Service"
 
-    # //tagName[@attribute=value]
-    # SelectorsHub.com - Xpath
-    # make_appointment_btn = driver.find_element(By.XPATH,"//a[@id='btn-make-appointment']")
+    
+    
     # make_appointment_btn_text = driver.find_element(By.XPATH,"//a[text()='Make Appointment']")
     make_appointment_btn_partial = driver.find_element(By.XPATH, "//a[contains(text(),'Make App')]")
 
-    # //a[text()="Make Appointment" and contains(@id,"btn-make-appointment")]
+    
 
 
 
 
     make_appointment_btn_partial.click()
-
-    # Make Appointment Button
-
-    # //a[@id='btn-make-appointment'] - Relative Direct
-
-    # //*[@id='btn-make-appointment'] - This is called * wild card ,
-    # Find id = btn-make-app in All the TagNames, this will be a Slow xpath
-
-    # XPATH Functions
-
-    # Full Visible Text Match
-    # Text() -> //a[text()='Make Appointment']
-
-    # Partial Match
-
-    # Contains
-    # // a[contains(text(), 'Make App')]
-    # // a[contains(text(), 'Make')]
 
     list_elements_p = driver.find_elements(By.XPATH, "//p[contains(text(),'A')]")
     for i in list_elements_p:
@@ -52,25 +33,7 @@ def test_open_login():
             i.click()
         print(i.text)
 
-    # Start-with
-    # //a[starts-with(text(),'Make')]
-    # End-With
-
-    # Abs - Not recommended - Never use it
-    # /html/body/header/div/a
-    # Start from the Root
-
-    # Xpath?  Relative or Abs?
-
-    # Find the p tag via the contains functions
-    # //p[contains(text(),'Copyright')]
-    # //p[contains(@class,'muted')]
-
-
-    # Xpath OR and And
-    # //p[contains(text(),'A') and contains(@class,"mute")] - Single element
-    # //p[contains(text(),'A') or contains(@class,"mute")]
-
+    
 
     ptag = driver.find_element(By.CLASS_NAME, "text-muted")
     print(ptag.text)
