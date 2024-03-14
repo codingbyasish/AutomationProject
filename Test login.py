aaf0496faaa7695b5196f584da60b8eb88a20f0f
codingbyasish <161559490+codingbyasish@users.noreply.github.com>
2024-03-14 18:41:59 +0530

    Create Test login.py

---
+++ Test login.py
@@ -0,0 +1,28 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
+def test_open_login():
+    driver = webdriver.Chrome()
+    driver.get("https://app.vwo.com")
+
+    email_input = driver.find_element(By.XPATH, "//p[@class='page-sub-title']")
+    pass_input = driver.find_element(By.CSS_SELECTOR, "[name='password']")
+    submit_input = driver.find_element(By.CSS_SELECTOR, "button[id='js-login-btn']")
+
+    email_input.send_keys("admin")
+    pass_input.send_keys("admin")
+
+    submit_input.click()
+
+
+
+
+
+
+
+    time.sleep(5)
+    driver.quit()
